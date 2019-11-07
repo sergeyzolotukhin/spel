36282b41da5c70335554849d08c71a61336cf01c
serhij.zolotukhin <SZolotukhin@luxoft.com>
2019-11-07 17:39:18 +0200

    Create SpEL example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spel
+
 Spring Expression Language
+
+(SpEL doc){https://docs.spring.io/spring/docs/current/spring-framework-reference/core.html#expressions}
--- src/main/java/ua/in/szolotukhin/spel/Application.java
+++ src/main/java/ua/in/szolotukhin/spel/Application.java
@@ -4,13 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 @Slf4j
 public class Application {
     public static void main(String[] args) {
+
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        context.setVariable("var", "Serhij Zolotukhin");
+
         ExpressionParser parser = new SpelExpressionParser();
-        Expression exp = parser.parseExpression("'Hello World'");
-        String message = (String) exp.getValue();
+        Expression exp = parser.parseExpression("'Hello World ' + #var");
+        String message = (String) exp.getValue(context);
 
         log.info("Expression evaluation: {}", message);
     }
