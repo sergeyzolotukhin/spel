ac597448db2ccc48469427446d58823affb11a67
serhij.zolotukhin <SZolotukhin@luxoft.com>
2019-11-07 17:27:01 +0200

    Create SpEL example

---
+++ src/main/java/ua/in/szolotukhin/spel/Application.java
@@ -0,0 +1,17 @@
+package ua.in.szolotukhin.spel;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+@Slf4j
+public class Application {
+    public static void main(String[] args) {
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp = parser.parseExpression("'Hello World'");
+        String message = (String) exp.getValue();
+
+        log.info("Expression evaluation: {}", message);
+    }
+}
